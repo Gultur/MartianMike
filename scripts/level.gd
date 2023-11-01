a7bccb301f5dd8710866a02fa90e7eb028bd6868
Gultur <n.kervinio@gmail.com>
2023-11-01 16:38:16 +0100

    added player with movement, setting camera limit

--- scripts/level.gd
+++ scripts/level.gd
@@ -7,8 +7,14 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
+
+
+func _on_death_zone_body_entered(body: CharacterBody2D):
+	body.velocity = Vector2.ZERO
+	body.global_position = $PlayerSpawnPoint.global_position
+
---
+++ scripts/player.gd
@@ -0,0 +1,81 @@
+extends CharacterBody2D
+class_name Player
+
+@export var gravity: float = 400.00
+@export var speed: int = 125
+@export var jump_force: int = 200
+
+var max_velocity: float = 500.00
+var jump_count: int = 0
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	if is_on_floor():
+		jump_count = 0
+		
+	if not is_on_floor():
+		velocity.y = min(velocity.y + gravity * delta, max_velocity)
+
+	if Input.is_action_just_pressed("jump") and can_player_jump(): #is_on_floor():
+		velocity.y -= jump_force
+		jump_count += 1
+		
+	var direction: float = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		var is_moving_left = (direction == -1)
+		animated_sprite.flip_h = is_moving_left #we only flip when player move left
+	velocity.x = direction * speed
+	
+	move_and_slide()
+	update_animation(direction)
+
+func update_animation(direction: float) -> void:
+	if is_on_floor():
+		if is_player_idle(direction):
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if is_player_jumping(velocity.y):
+			print("jump")
+			animated_sprite.play("jump")
+		else:
+			print("fall")
+			animated_sprite.play("fall")
+	
+func can_player_jump() -> bool:
+	return jump_count < 2
+	
+func is_player_idle(direction: float) -> bool:
+	return (direction == 0)
+	
+func is_player_jumping(velocity_y: float) -> bool:
+	return velocity_y < 0
+	
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+#
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
